73835094194e70de21157da988ac9062deaa12d6
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-04-04 12:01:02 +0100

    second pub test passing

--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -9,11 +9,14 @@ class TestPub < MiniTest::Test
     @drink2 = Drink.new("Wine", 12.0)
     @drink3 = Drink.new("Beer", 20.0)
     @cellar = [@drink1, @drink2, @drink3]
-    @pub = Pub.new("The Ivy", 0.0, @cellar)
+    @pub = Pub.new("The Ivy", 10.0, @cellar)
   end
 
   def test_get_pub_name()
     assert_equal("The Ivy", @pub.name)
+  end
+  def test_get_pub_till()
+    assert_equal(10.0, @pub.till)
   end
 
 end
